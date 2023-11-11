214393219c21c7bd2b2d4c770f5fe64034f725a7
MarianRol <mace2101@hotmail.com>
2023-11-11 10:40:48 -0300

    estilo al carrusel

--- src/Components/CarouselBurguer.js
+++ src/Components/CarouselBurguer.js
@@ -17,9 +17,13 @@ const CarouselBurguer = () => {
         alt="Cabra Bomba"
       />
       
-    <Carousel.Caption>
-        <h5>Cabra Bomba</h5>
+    <Carousel.Caption >
+    <div className='estiloCaption'>
+    
+    <h4>Cabra Bomba</h4>
         <p>Combo: papas fritas, guacamole y gaseosa</p>
+    </div>
+      
       </Carousel.Caption>      
     
     </Carousel.Item>
@@ -30,10 +34,15 @@ const CarouselBurguer = () => {
         src="https://prior-client-images.s3.eu-west-2.amazonaws.com/burgermaker/e3c27f9f78a74c0aa95a85979516ed81.png"
         alt="Cabra Doble"
       />
-      <Carousel.Caption>
-        <h5>Hambuguesa Doble Extra Chedar</h5>
+      <Carousel.Caption >
+      <div className='estiloCaption'>
+           <h4>Cabra Doble Extra Chedar</h4>
         <p>Combo: papas fritas y gaseosa</p>
+      </div>    
+      
       </Carousel.Caption>
+  
+ 
    
     </Carousel.Item>
     <Carousel.Item>
@@ -43,11 +52,14 @@ const CarouselBurguer = () => {
         src="https://prior-client-images.s3.eu-west-2.amazonaws.com/burgermaker/1e5ae71171f3065a839be04ae371619c.png"
         alt="Cabra Fondo"
       />
-      <Carousel.Caption>
-        <h5>Cabra Fondo</h5>
+      <Carousel.Caption >
+        <div className='estiloCaption'>
+          <h4>Cabra Fondo</h4>
         <p>
         Combo: papas fritas y agua mineral
         </p>
+        </div>
+    
       </Carousel.Caption>
       
     </Carousel.Item>
@@ -58,10 +70,12 @@ const CarouselBurguer = () => {
         src="https://prior-client-images.s3.eu-west-2.amazonaws.com/burgermaker/42b64213c5083d3424a4b620a2e22165.png"
         alt="Cabra Simple"
       />
-      <Carousel.Caption>
-        <h5>Cabra Simple</h5>
+      <Carousel.Caption >
+      <div className='estiloCaption'> 
+        <h4>Cabra Simple</h4>
         <p>Combo: papas rusticas y jugo de naranja</p>
-      </Carousel.Caption>
+      </div>
+        </Carousel.Caption>
     
     </Carousel.Item>
 
@@ -73,9 +87,12 @@ const CarouselBurguer = () => {
         src="https://theburgermaker.com/wp-content/uploads/2022/08/gabraloca.png"
         alt="Cabra Completa"
       />
-      <Carousel.Caption>
-        <h5>Cabra Completa</h5>
-        <p>Combo: papas fritas y cerveza</p>
+      <Carousel.Caption >
+        <div className='estiloCaption'>
+            <h4>Cabra Completa</h4>
+            <p>Combo: papas fritas y cerveza</p>
+        </div>
+
       </Carousel.Caption>
     
     </Carousel.Item>
